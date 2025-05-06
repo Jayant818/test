df6faf73be30b2fbce150f0d42e73dce4e851493
jayant818 <jayant2@s.amity.edu>
2025-05-06 16:55:50 +0530

    Refactor route handler in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/test', (req, res) => {
-  console.log('Route Hit');
-  res.send('Test conifirmed');
+app.get("/test", (req, res) => {
+  console.log("Route Hit");
+  res.send("Test conifirmed");
+});
+
+app.get("/test2", (req, res) => {
+  console.log("Route Hit2");
+  res.send("Test conifirmed2");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
