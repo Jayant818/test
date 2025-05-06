7215ca6a9c555540e99b028f1d5d0a1e8afdefda
jayant818 <jayant2@s.amity.edu>
2025-05-06 13:43:33 +0530

    Add /test route to the server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.get("/test", (req, res) => {
+  console.log("Route Hit");
+  res.send("Test conifirmed");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
